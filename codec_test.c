bec976de2e056038c7564ca71d61cc7825f113df
cch <cch@rock-chips.com>
2012-11-12 11:23:54 +0800

    change the codec test format

--- codec_test.c
+++ codec_test.c
@@ -9,16 +9,19 @@
 
 #define AUDIO_HW_OUT_PERIOD_MULT 8 // (8 * 128 = 1024 frames)
 #define AUDIO_HW_OUT_PERIOD_CNT 4
-
+#define FILE_PATH "/sbin/codectest.pcm"
+#define REC_DUR 3 //the unit is second
 pthread_t codec_tid;  
 int codec_err = -1;
 
 void* rec_play_test(void *argv)
 {		
-	struct pcm* pcmIn;
-	struct pcm* pcmOut;
+	struct pcm* pcmIn = NULL;
+	struct pcm* pcmOut = NULL;
+	FILE *fp = NULL;
 	unsigned bufsize;
 	char *data;
+	char recData[4*REC_DUR*44100];
 
 	unsigned inFlags = PCM_IN;
 	unsigned outFlags = PCM_OUT;
@@ -29,36 +32,81 @@ void* rec_play_test(void *argv)
 
 	inFlags |= flags;
 	outFlags |= flags;
+	
+	fp = fopen(FILE_PATH,"rb");
+		if(NULL == fp)
+			goto fail;
+		
+	
+	
+	pcmOut = pcm_open(outFlags);
+	if (!pcm_ready(pcmOut)) {
+		pcm_close(pcmOut);
+		pcmOut = NULL;
+		goto fail;
+	}
+	
+	bufsize = pcm_buffer_size(pcmOut);
+	
+repeat:	
+	fseek(fp,0,SEEK_SET);
+	
+	while(bufsize == fread(recData,1,bufsize,fp)){
+		if (pcm_write(pcmOut, recData, bufsize)) {
+		fprintf(stderr,"could not write %d bytes\n", bufsize);
+		goto fail;
+		}
+	}
+	
+	pcm_close(pcmOut);
+	pcmOut = NULL;
 
 	pcmIn = pcm_open(inFlags);
 	if (!pcm_ready(pcmIn)) {
-	pcm_close(pcmIn);
-	goto fail;
+		pcm_close(pcmIn);
+		pcmIn = NULL;
+		goto fail;
+	}
+	
+	bufsize = pcm_buffer_size(pcmIn);
+	
+	data = recData;
+	while (!pcm_read(pcmIn, data, bufsize)) {
+		data += bufsize;
+		if(data + bufsize - recData >= 4*REC_DUR*44100)
+		{
+			pcm_close(pcmIn);
+			pcmIn = NULL;
+			break;
+		}		
 	}
 
 	pcmOut = pcm_open(outFlags);
 	if (!pcm_ready(pcmOut)) {
-	pcm_close(pcmOut);
-	goto fail;
+		pcm_close(pcmOut);
+		pcmOut = NULL;
+		goto fail;
 	}
+	
+	bufsize = pcm_buffer_size(pcmOut);
 
-	bufsize = pcm_buffer_size(pcmIn);
-	data = malloc(bufsize);
-	if (!data) {
-	fprintf(stderr,"could not allocate %d bytes\n", bufsize);
-	return -1;
+	data = recData;
+	while (!pcm_write(pcmOut, data, bufsize)) {
+		data += bufsize;
+		if(data + bufsize - recData >= 4*REC_DUR*44100)
+		{
+			fprintf(stderr,"now goto repeat\n");
+			goto repeat;
+		}		
 	}
-
-	while (!pcm_read(pcmIn, data, bufsize)) {
-	if (pcm_write(pcmOut, data, bufsize)) {
-	fprintf(stderr,"could not write %d bytes\n", bufsize);
-	return -1;
-	}
-	}
-
-	fail:
-	pcm_close(pcmIn);
-	pcm_close(pcmOut);
+	
+fail:
+	if(pcmIn)
+		pcm_close(pcmIn);
+	if(pcmOut)
+		pcm_close(pcmOut);
+	if(fp)
+		fclose(fp);
 	return 0;
 }
 
