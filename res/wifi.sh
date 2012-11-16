4fba6a0979a199697731f69e78ec5333db81a1ec
hwg <hwg@rock-chips.com>
2012-11-16 20:23:08 +0800

    modify wifi test script

--- res/wifi.sh
+++ res/wifi.sh
@@ -9,6 +9,8 @@ result_file=/data/scan_result.txt
 chip_type_path=/sys/class/rkwifi/chip
 module_path=$module_path_wlan
 
+jmax=1
+
 if busybox cat $chip_type_path | busybox grep RK903; then
   module_path=$module_path_rk903
 fi
@@ -22,29 +24,34 @@ if busybox cat $chip_type_path | busybox grep BCM4330; then
 fi
 
 if busybox cat $chip_type_path | busybox grep RTL8188CU; then
+  jmax=6
   module_path=$module_path_8192cu
 fi
 
 if busybox cat $chip_type_path | busybox grep RTL8188EU; then
+  jmax=6
   module_path=$module_path_8188eu
 fi
 
 if busybox cat $chip_type_path | busybox grep RT5370; then
+  jmax=6
   module_path=$module_path_rt5370
-fi
-
-echo "insmod $module_path"
-insmod "$module_path"
-if [ $? -ne 0 ]; then
-    echo "insmod failed"
-    exit 0
 fi
 
 echo "touch $result_file"
 busybox touch $result_file
 
+j=0
+
 echo "get scan results"
-for i in 1 2 3 4 5 6; do
+while [ $j -lt $jmax ]; 
+do
+    echo "insmod $module_path"
+    insmod "$module_path"
+    if [ $? -ne 0 ]; then
+        echo "insmod failed"
+        exit 0
+    fi
 
     echo "sleep 3s"
     busybox sleep 3
@@ -62,13 +69,11 @@ for i in 1 2 3 4 5 6; do
         exit 1
     fi
 
-    #if [ $i -eq 2 -o $i -eq 4 ]; then
-        echo "rmmod wlan"
-        rmmod wlan
-        busybox sleep 1
-        echo "insmod $module_path"
-        insmod "$module_path"
-    #fi
+    echo "rmmod wlan"
+    rmmod wlan
+    busybox sleep 1
+    
+    j=$((j+1))
 done
 
 echo "wlan test failed"
