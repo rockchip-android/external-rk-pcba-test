ad4b84cc6865c3f97cc9db18989890e77f499dc6
cch <cch@rock-chips.com>
2012-12-20 11:09:44 +0800

    add some log for codec test

--- codec_test.c
+++ codec_test.c
@@ -34,27 +34,30 @@ void* rec_play_test(void *argv)
 	outFlags |= flags;
 	
 	fp = fopen(FILE_PATH,"rb");
-		if(NULL == fp)
-			goto fail;
-		
-	
+	if(NULL == fp)
+	{
+		fprintf(stderr,"could not open %s file, will go to fail\n", FILE_PATH);
+		goto fail;
+	}
+				
 	
 	pcmOut = pcm_open(outFlags);
 	if (!pcm_ready(pcmOut)) {
+		fprintf(stderr,"the first time pcmOut open but the not ready,will go to fail\n");
 		pcm_close(pcmOut);
 		pcmOut = NULL;
 		goto fail;
 	}
 	
 	bufsize = pcm_buffer_size(pcmOut);
-	
+	usleep(10000);
 repeat:	
 	fseek(fp,0,SEEK_SET);
 	
 	while(bufsize == fread(recData,1,bufsize,fp)){
 		if (pcm_write(pcmOut, recData, bufsize)) {
-		fprintf(stderr,"could not write %d bytes\n", bufsize);
-		goto fail;
+			fprintf(stderr,"the pcmOut could not write %d bytes file data,will go to fail\n", bufsize);
+			goto fail;
 		}
 	}
 	
@@ -63,13 +66,14 @@ repeat:
 
 	pcmIn = pcm_open(inFlags);
 	if (!pcm_ready(pcmIn)) {
+		fprintf(stderr,"the pcmIn open but the not ready,will go to fail\n");
 		pcm_close(pcmIn);
 		pcmIn = NULL;
 		goto fail;
 	}
 	
 	bufsize = pcm_buffer_size(pcmIn);
-	
+	usleep(10000);
 	data = recData;
 	while (!pcm_read(pcmIn, data, bufsize)) {
 		data += bufsize;
@@ -80,16 +84,23 @@ repeat:
 			break;
 		}		
 	}
+	
+	if(data + bufsize - recData < 4*REC_DUR*44100)
+	{
+			fprintf(stderr,"the pcmIn could not be read\n");
+			goto fail;
+	}
 
 	pcmOut = pcm_open(outFlags);
 	if (!pcm_ready(pcmOut)) {
+		fprintf(stderr,"the second time pcmOut open but the not ready,will go to fail\n");
 		pcm_close(pcmOut);
 		pcmOut = NULL;
 		goto fail;
 	}
 	
 	bufsize = pcm_buffer_size(pcmOut);
-
+	usleep(10000);
 	data = recData;
 	while (!pcm_write(pcmOut, data, bufsize)) {
 		data += bufsize;
@@ -99,6 +110,7 @@ repeat:
 			goto repeat;
 		}		
 	}
+	fprintf(stderr,"the pcmOut could not write record data ,will go to fail\n");
 	
 fail:
 	if(pcmIn)
