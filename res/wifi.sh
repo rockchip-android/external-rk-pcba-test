518a2c1df1c8096a3a3f33a1e8cafc1d220f93b8
hwg <hwg@rock-chips.com>
2013-01-24 10:14:56 +0800

    modify wifi pcba test, support rssi info display

--- res/wifi.sh
+++ res/wifi.sh
@@ -6,21 +6,30 @@ module_path_rk903=/res/rkwifi.ko
 module_path_rt5370=/res/rt5370sta.ko
 module_path_wlan=/res/wlan.ko
 result_file=/data/scan_result.txt
+result_file2=/data/scan_result2.txt
 chip_type_path=/sys/class/rkwifi/chip
+pcba_node=/sys/class/rkwifi/pcba
 module_path=$module_path_wlan
+chip_broadcom=false
 
 jmax=1
 
 if busybox cat $chip_type_path | busybox grep RK903; then
   module_path=$module_path_rk903
+  chip_broadcom=true
+  echo 1 > $pcba_node
 fi
 
 if busybox cat $chip_type_path | busybox grep RK901; then
   module_path=$module_path_rk903
+  chip_broadcom=true
+  echo 1 > $pcba_node
 fi
 
 if busybox cat $chip_type_path | busybox grep BCM4330; then
   module_path=$module_path_rk903
+  chip_broadcom=true
+  echo 1 > $pcba_node
 fi
 
 if busybox cat $chip_type_path | busybox grep RTL8188CU; then
@@ -62,9 +71,16 @@ do
         #    echo "ifconfig wlan0 up failed"
         #    exit 0
         #fi
-        
-        iwlist wlan0 scan | busybox grep SSID > $result_file
+    
+        iwlist wlan0 scanning
+        if [ $chip_broadcom = "true" ]; then
+            echo "sleep 3s"
+            busybox sleep 3    
+        fi
+        iwlist wlan0 scanning last | busybox grep SSID > $result_file
         busybox cat $result_file
+        iwlist wlan0 scanning last | busybox grep "Signal level" > $result_file2
+        busybox cat $result_file2
         echo "success"
         exit 1
     fi
