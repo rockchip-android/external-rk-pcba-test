b542e86f618eb72879659e4e97f2c2c9761b74d3
Dayao Ji <jdy@rock-chips.com>
2015-06-23 15:58:20 +0800

    pcba:sofia3gr: add udisk test item.
    udisk test is de-actived in default, to enable it, please config res/test_config_sofia.cfg:
    [udisk]
    display_name= "Udisk"
    activated   = 0  // change this value from 0 to 1 to enable udisk test
    program     = "udisktester.sh"
    category    = 1
    run_type    = 1
    
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- res/udisktester_sofia.sh
+++ res/udisktester_sofia.sh
@@ -4,13 +4,21 @@
 #LOG_FILE="/data/udisk.log"
 #source send_cmd_pipe.sh
 
+result_file_udisk=/data/udisk_capacity.txt
+
+su root
+
+if [ -e $result_file2 ] ; then
+busybox rm -f $result_file_udisk
+fi
+
 #while true; do
     for nr in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
         udisk="/dev/block/sd$nr"
         udiskp=$udisk"1"
         part=$udisk
     
-        #echo "searching disk ..." >> LOG_FILE
+        echo "searching disk ..."
         while true; do
             while true; do
                 if [ -b "$udisk" ]; then
@@ -24,16 +32,16 @@
                 fi
             done
             
-            if [ ! -d "/tmp/udisk" ]; then
-                busybox mkdir -p /tmp/udisk
+            if [ ! -d "/mnt/usb_storage" ]; then
+                busybox mkdir -p /mnt/usb_storage
             fi
             
-            #echo "mounting disk ..." >> LOG_FILE
-            busybox mount -t vfat $udisk /tmp/udisk
+            echo "mounting disk ..."
+            mount -t vfat $udisk /mnt/usb_storage
             if [ $? -ne 0 ]; then
-                busybox mount -t vfat $udiskp /tmp/udisk
+                mount -t vfat $udiskp /mnt/usb_storage
                 if [ $? -ne 0 ]; then
-                    echo "udisk mount failed" >> LOG_FILE
+                    echo "udisk mount failed"
                     exit 1
                     #SEND_CMD_PIPE_FAIL $3
                     #busybox sleep 3
@@ -48,10 +56,10 @@
             break
         done
     
-        capacity=`busybox df | busybox grep /tmp/udisk | busybox awk '{printf $2}'`
-        #echo "$part: $capacity" >> LOG_FILE
+        capacity=`df | grep "/mnt/usb_storage" | busybox awk '{printf $2}'`
+        echo "udisk $part: $capacity"
 
-        busybox umount /tmp/udisk
+        umount /mnt/usb_storage
         #SEND_CMD_PIPE_OK_EX $3 $capacity
 
         echo $capacity > /data/udisk_capacity.txt
