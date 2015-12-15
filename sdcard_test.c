ec9b8e93ba67efcdeafd0911a31785e2d3803a3a
Zhangbin Tong <zebulun.tong@rock-chips.com>
2015-12-15 18:11:02 +0800

    pcba: sdcard_test: delete sofia sdcard code
    Change-Id: I01436e2b65a60249764933310fd829714df6e34c
    Signed-off-by: Zhangbin Tong <zebulun.tong@rock-chips.com>

--- sdcard_test.c
+++ sdcard_test.c
@@ -6,7 +6,7 @@
 #include "test_case.h"
 #include "language.h"
 
-#define LOG_TAG	"[PCBA,SDCARD]: "
+#define LOG_TAG	"[sdcard]: "
 #define LOG(x...)	printf(LOG_TAG x)
 
 #define SCAN_RESULT_LENGTH	128
@@ -27,55 +27,20 @@ void *sdcard_test(void *argv)
 	y = tc_info->y;
 
 	LOG("start sdcard test.\n");
-#ifdef SOFIA3GR_PCBA
-	ui_print_xy_rgba(0, y, 255, 255, 0, 255, "%s:[%s..]\n", PCBA_SDCARD,
-			 PCBA_TESTING);
-#else
+
 	ui_print_xy_rgba(0, y, 255, 255, 0, 255, "%s\n", PCBA_SDCARD);
-#endif
 
 #if defined(RK3288_PCBA) || defined(RK3368_PCBA)
 	ret = __system("busybox chmod 777 /res/emmctester.sh");
-#elif defined(SOFIA3GR_PCBA)
-	/* sofia3gr process empty */
 #else
 	ret = __system("busybox chmod 777 /res/mmctester.sh");
 #endif
 
-#ifndef SOFIA3GR_PCBA
 	if (ret)
 		LOG("chmod mmctester.sh failed :%d\n", ret);
-#endif
 
 #if defined(RK3288_PCBA) || defined(RK3368_PCBA)
 	ret = __system("/res/emmctester.sh");
-#elif defined(SOFIA3GR_PCBA)
-	int test_counts = 0;
-
-	while (1) {
-		fp = NULL;
-		LOG("%s::wait for insert sd card...\n", __func__);
-		if (__system("/system/bin/mmctester.sh") > 0)
-			fp = fopen(SCAN_RESULT_FILE, "r");
-
-		if (fp != NULL)
-			break;
-
-		if (test_counts++ > 3) {
-			LOG("can not open %s.\n", SCAN_RESULT_FILE);
-			ui_print_xy_rgba(0, y, 255, 0, 0, 255, "%s:[%s]\n",
-					 PCBA_SDCARD, PCBA_FAILED);
-			tc_info->result = -1;
-			return argv;
-		}
-		sleep(1);
-	}
-	ui_print_xy_rgba(0, y, 0, 255, 0, 255, "%s:[%s]\n", PCBA_SDCARD,
-			 PCBA_SECCESS);
-	tc_info->result = 0;
-	fclose(fp);
-
-	return argv;
 #else
 	ret = __system("/res/mmctester.sh");
 #endif
